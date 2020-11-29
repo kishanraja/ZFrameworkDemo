ac2410e21659109e214cdc3039faec356681fd37
kishan <rajakishanrk1996@gmail.com>
2020-11-29 17:30:59 +0530

    first commit

---
+++ ZFrameworkDemo.xcframework/ios-x86_64-simulator/ZFrameworkDemo.framework/Headers/ZFrameworkDemo.h
@@ -0,0 +1,18 @@
+//
+//  ZFrameworkDemo.h
+//  ZFrameworkDemo
+//
+//  Created by KISHAN_RAJA on 29/11/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZFrameworkDemo.
+FOUNDATION_EXPORT double ZFrameworkDemoVersionNumber;
+
+//! Project version string for ZFrameworkDemo.
+FOUNDATION_EXPORT const unsigned char ZFrameworkDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZFrameworkDemo/PublicHeader.h>
+
+
---
+++ ZFrameworkDemo/ZFrameworkClass.swift
@@ -0,0 +1,18 @@
+//
+//  ZFrameworkClass.swift
+//  ZFrameworkDemo
+//
+//  Created by KISHAN_RAJA on 29/11/20.
+//
+
+import Foundation
+
+public class ZFrameworkClass {
+    public init () {
+        
+    }
+    
+    public func classFuncation() {
+        print(#function)
+    }
+}
